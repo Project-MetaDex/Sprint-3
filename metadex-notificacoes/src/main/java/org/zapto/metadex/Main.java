b7f0d1a054795fb910a6d3732c367644a05a9f44
Gle1sonFreitas <gleison.freitas@sptech.school>
2026-05-31 18:24:34 -0300

    JavaMail finalizado e configurado

---
+++ metadex-notificacoes/src/main/java/org/zapto/metadex/Main.java
@@ -0,0 +1,35 @@
+package org.zapto.metadex;
+
+import org.zapto.metadex.config.ConnectionFactory;
+import org.zapto.metadex.config.EmailConfig;
+import org.zapto.metadex.service.EmailService;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        EmailConfig emailConfig = new EmailConfig();
+        EmailService emailService = new EmailService(emailConfig);
+
+        String sql = "SELECT email FROM usuario";
+
+        List<String> email = ConnectionFactory.createConnection().queryForList(sql, String.class);
+
+        for (String s : email) {
+            emailService.enviarEmail(
+                    s,
+                    "Novos conteúdos na plataforma Metadex",
+                    "Olá,\n" +
+                            "\n" +
+                            "As bases de dados do Metadex foram atualizadas, cheque as novidades.\n" +
+                            "\n" +
+                            "O processamento da carga de metas referente ao mês corrente foi finalizado com sucesso. Todas as tabelas operacionais, históricos de performance e painéis de indicadores foram atualizados.\n" +
+                            "\n" +
+                            "Para checar os novos dados inseridos, acesse a plataforma.\n" +
+                            "\n" +
+                            "Atenciosamente,\n" +
+                            "Metadex"
+            );
+        }
+    }
+}
---
+++ metadex-notificacoes/src/main/java/org/zapto/metadex/service/EmailService.java
@@ -0,0 +1,36 @@
+package org.zapto.metadex.service;
+
+import org.zapto.metadex.config.EmailConfig;
+
+import javax.mail.Message;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EmailService {
+
+    private final EmailConfig emailConfig;
+
+    public EmailService(EmailConfig emailConfig) {
+        this.emailConfig = emailConfig;
+    }
+
+    public void enviarEmail(String destinatario, String assunto, String corpo){
+        try {
+            MimeMessage message = new MimeMessage(emailConfig.criarSessao());
+
+            message.setFrom(new InternetAddress(emailConfig.getRemetente()));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destinatario));
+            message.setSubject(assunto);
+            message.setText(corpo);
+
+            Transport.send(message);
+
+
+        }catch (Exception e) {
+            throw new RuntimeException("Falha ao enviar e-mail de notificação", e);
+        }
+
+
+    }
+}
