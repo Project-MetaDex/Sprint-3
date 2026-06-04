ce35bd6f4216a1ee578ef8afd48544c2612e52d5
JosueAlvarezAvendano <josue.avendano@sptech.school>
2026-06-04 11:21:08 -0300

    Iniciando a integração da tela Perfil Mentor

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -66,7 +66,7 @@ function listarAlunos(idMentor) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarAlunos():", idMentor);
 
     var instrucaoSql = `
-        SELECT idUsuario, nome, posicaoRanking FROM usuario WHERE fkMentor = ${idMentor};
+        SELECT idUsuario, nome, posicaoRanking, ROUND(((qtdVitorias / totalBatalhas) * 100), 2) AS winRate FROM usuario WHERE fkMentor = ${idMentor};
     `;
     console.log("Listando alunos do Mentor: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
