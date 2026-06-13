c55bf852292f7d9dcf6b2977a016cb8cab72d7e8
JosueAlvarezAvendano <josue.avendano@sptech.school>
2026-06-13 11:18:12 -0300

    Ajustes

--- src/models/showdownModel.js
+++ src/models/showdownModel.js
@@ -1,7 +1,7 @@
 var database = require("../database/config")
 
 function dadosShowdown(idUsuario) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function pokemonMaisUsados():", idUsuario);
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function dadosShowdown():", idUsuario);
 
     var instrucaoSql = `
         SELECT DATE_FORMAT(dataPartida, '%b') AS mes,
